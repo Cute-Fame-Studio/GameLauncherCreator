98e7d0411cd2786df83e0bdd0d13bf0ec8e81503
lumi <94462076+lumi2021@users.noreply.github.com>
2023-09-29 22:20:26 -0300

    main info prototipe

--- TestingProject/src/app/components/info/info-component.js
+++ TestingProject/src/app/components/info/info-component.js
@@ -1,11 +1,18 @@
+import { Component } from "../../../util/component.js";
+
 export class InfoComponent extends Component { 
+
+    template = "components/info/info-component.html";
+
     constructor(_elmRef, _core)
     {
-
+        super();
+        this.element = _elmRef,
+        this.core = _core;
     } 
   
-    ready(ready)
+    ready()
     {
-
-    } 
+        this.loadTemplate();
+    }
 } 
